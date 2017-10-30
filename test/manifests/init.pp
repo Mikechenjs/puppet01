55271ed569e333edc93830fc38544ea283f9b63a
root <root@puppet01.gbnc.com>
2017-10-30 11:00:40 +0800

    change test/manifests/init.pp 2017103004

--- test/manifests/init.pp
+++ test/manifests/init.pp
@@ -2,14 +2,6 @@ class test{
 file {"/tmp/$hostname.txt":content =>"hello world!";}
 }
 
-node docker01.gbnc.com {
-	include nginx
-}
-
-node apache01.gbnc.com{
-	include httpd
-}
-
 node 'default' {
 	notify{"this is a test"}
  }
